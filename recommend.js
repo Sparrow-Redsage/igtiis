040e876a01d40e3a2ff9cd3ab532c3b65cac0534
Venzah Hamilton <vham2952@gmail.com>
2026-05-28 17:18:21 -0700

    Now treats list like FIFO queue when user selects button for show recommendation

--- recommend.js
+++ recommend.js
@@ -3,16 +3,17 @@ const h1 = document.querySelector("h1"), img = document.getElementById("show-pos
 let showList = [];
 
 if(localStorage.getItem('shows')) {
+        let localStorageJson = JSON.parse(localStorage.getItem('shows'));
+        showList = localStorageJson;
+        show = showList.shift();
+
+        console.log("Show:", show);
         console.log("Show list:", showList);
-        let localStorageJson = localStorage.getItem('shows');
-        showList = JSON.parse(localStorageJson);
-        //show = showList.shift();
-        console.log("Show list:", showList);
 
 
-        //h1.innerHTML = showList.title;
-        //img.src = showList.photo;
+        h1.innerHTML = show.title;
+        img.src = show.photo;
 
-        //const json = JSON.stringify(showList);
-        //localStorage.setItem('shows', json);
+        const json = JSON.stringify(showList);
+        localStorage.setItem('shows', json);
     }
