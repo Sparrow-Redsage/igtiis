91829de9c858d7b11a29bb09451b118a04e65f8f
Venzah Hamilton <vham2952@gmail.com>
2026-05-28 18:48:49 -0700

    Fixed issue where clear function doesn't work after immediately adding a show

--- submit.js
+++ submit.js
@@ -1,8 +1,7 @@
 const form = document.querySelector('form'), recommend = document.querySelector('.recommend-button'), clear = document.querySelector('.clear-button');
 
-let showList = [];
-
 form.addEventListener('submit', (e) => {
+    let showList = [];
     if(localStorage.getItem('shows')) {
         let localStorageJson = JSON.parse(localStorage.getItem('shows'));
         showList = localStorageJson;
@@ -20,15 +19,24 @@ form.addEventListener('submit', (e) => {
 });
 
 recommend.addEventListener('click', (e) => {
+    let showList = [];
     if(localStorage.getItem('shows')) {
         showList = JSON.parse(localStorage.getItem('shows'));
+            if(showList.length === 0) {
+            window.alert('No shows to recommend! Add a show first!');
+            e.preventDefault();
     }
-    if(showList.length === 0) {
+    }
+    else{
         window.alert('No shows to recommend! Add a show first!');
         e.preventDefault();
     }
+
 });
 
 clear.addEventListener('click', (e) => {
+    let showList = [];
     localStorage.removeItem('shows');
+    const json = JSON.stringify(showList);
+    localStorage.setItem('shows', json);
 });
