4643a68e4cc0d3df2db48ff353230c33012041bd
Venzah Hamilton <vham2952@gmail.com>
2026-05-28 18:58:57 -0700

    Added different behavior based on whether or not the optional photo link is submitted with the form

--- recommend.js
+++ recommend.js
@@ -1,4 +1,4 @@
-const h1 = document.querySelector("h1"), img = document.getElementById("show-poster");
+const h1 = document.querySelector("h1"), img = document.getElementById("image");
 
 let showList = [];
 
@@ -6,7 +6,11 @@ showList = JSON.parse(localStorage.getItem('shows'));
 show = showList.shift();
 
 h1.innerHTML = show.title;
-img.src = show.photo;
+
+if(show.photo.length > 0){
+    img.insertAdjacentHTML('afterbegin', '<img src="' + show.photo + '" alt="Show poster">');
+}
 
 const json = JSON.stringify(showList);
 localStorage.setItem('shows', json);
+
