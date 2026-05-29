652eff588a464a7c453b61cbb576827102f9a9b3
Venzah Hamilton <vham2952@gmail.com>
2026-05-28 17:36:53 -0700

    Removed extra code

--- recommend.js
+++ recommend.js
@@ -2,18 +2,11 @@ const h1 = document.querySelector("h1"), img = document.getElementById("show-pos
 
 let showList = [];
 
-if(localStorage.getItem('shows')) {
-        let localStorageJson = JSON.parse(localStorage.getItem('shows'));
-        showList = localStorageJson;
-        show = showList.shift();
+showList = JSON.parse(localStorage.getItem('shows'));
+show = showList.shift();
 
-        console.log("Show:", show);
-        console.log("Show list:", showList);
+h1.innerHTML = show.title;
+img.src = show.photo;
 
-
-        h1.innerHTML = show.title;
-        img.src = show.photo;
-
-        const json = JSON.stringify(showList);
-        localStorage.setItem('shows', json);
-    }
+const json = JSON.stringify(showList);
+localStorage.setItem('shows', json);
